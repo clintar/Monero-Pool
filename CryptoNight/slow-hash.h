f325b92cab307bb512af1cb1f77e72ed272f8982
clintar <clint@gurutechnologies.net>
2014-05-13 13:30:54 -0600

    don't know if it makes any difference, but added attribute

--- CryptoNight/slow-hash.h
+++ CryptoNight/slow-hash.h
@@ -1,3 +1,5 @@
 #ifdef __WIN32
 extern "C" __declspec(dllexport) void cn_slow_hash(char *data, size_t length, char *hash);
+#else
+extern "C" __attribute__ ((dllexport)) void cn_slow_hash(char *data, size_t length, char *hash);
 #endif
